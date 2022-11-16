cac483276f551b6d0b546727b41193dc8d1ba037
JordyAraujo <jordyaraujo@outlook.com>
2022-11-16 14:39:57 -0300

    Feature: First functional test
    - Now using unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        # O usuário abre o navegador
+        self.browser = webdriver.Firefox()
 
-assert 'worked' in browser.title
+    def tearDown(self):
+        # O usuário fecha o navegador
+        return self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # O usuário acessa a aplicação
+        self.browser.get('http://localhost:8000')
+
+        # Confere o título da página
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
