432a7e5312a39e32c6a98183c51bdbfa7312af24
JordyAraujo <jordyaraujo@outlook.com>
2022-11-17 16:29:24 -0300

    Feature: Functional tests now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,8 +18,32 @@ class NewVisitorTest(unittest.TestCase):
         # O usuário acessa a aplicação
         self.browser.get('http://localhost:8000')
 
-        # Confere o título da página
+        # Confere o título e o cabeçalho da página
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Ela é convidada a adicionar um item à lista
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # Ela adiciona "Comprar penas de pavão" a uma caixa de texto
+        inputbox.send_keys('Buy peacock feathers')
+
+        # Quando ela pressiona Enter, a página atualiza, agora listando
+        # "1: Buy peacock feathers"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
