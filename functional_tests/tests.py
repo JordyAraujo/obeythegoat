e1011a5d4736159bc740b065384571d59faa6719
JordyAraujo <jordyaraujo@outlook.com>
2022-11-18 17:17:55 -0300

    Refactor: Functional tests
    - Make functional_tests an app
    - Use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     
     def setUp(self):
         # O usuário abre o navegador
@@ -24,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # O usuário acessa a aplicação
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # Confere o título e o cabeçalho da página
         self.assertIn('To-Do', self.browser.title)
@@ -60,7 +60,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
-
-
-if __name__ == '__main__':
-    unittest.main()
