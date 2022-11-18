7a89d89229391403b723c925466ccc3f48d214a5
JordyAraujo <jordyaraujo@outlook.com>
2022-11-18 14:42:41 -0300

    Feature: Receive form data from the browser

--- functional_tests.py
+++ functional_tests.py
@@ -23,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
 
-        # Ela é convidada a adicionar um item à lista
+        # Existe uma caixa de texto para adicionar um item à lista
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
@@ -40,9 +40,22 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+        # Ainda há uma caixa de texto para adicionar um item à lista
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        # Ela adiciona mais um item
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # A página atualiza novamente, agora mostrando os dois itens
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
         )
 
         self.fail('Finish the test!')
